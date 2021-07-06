f775e6c55393d26543a87c9eb55a2d78c34e0718
Oluwatofunmi Oyetan <tofunmi.oyetan@uky.edu>
2021-07-06 18:31:43 +0000

    project git mob setup

---
+++ DuoBuddy.py
@@ -0,0 +1,18 @@
+import requests
+import duolingo
+import pandas as pd
+
+def get_input():
+    username = str(input("Enter duolingo username: "))
+    password = str(input("Enter duolingo password: "))
+    return duolingo.Duolingo(username, password)
+
+info = get_input() #user name should be in strings...info
+user_information = info.get_user_info()
+full_name = user_information['fullname']
+
+print(full_name)
+
+
+
+ 
